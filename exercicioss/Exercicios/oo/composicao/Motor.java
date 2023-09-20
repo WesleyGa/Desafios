ffb202fd67db113e23e313753318d9bd12712c79
wesleygah <wes16062002@gmail.com>
2023-09-20 17:10:14 -0300

    revisão sobre composição

--- exercicioss/Exercicios/oo/composicao/Motor.java
+++ exercicioss/Exercicios/oo/composicao/Motor.java
@@ -1,21 +1,23 @@
 package oo.composicao;
 
 public class Motor {
-
+// um motor tem um carro
 	Carro carro;
+
 	
+	Motor(Carro carro) { // um motor tem um carrro
+		this.carro = carro;
+	}
+
 	boolean ligado = false;
 	double fatorInjecao = 1;
 
-	Motor(Carro carro){
-		this.carro = carro;
-	}
-	
-	int gitros() {
+	// rotações por minutos
+	int rpm() {
 
-		if (!ligado) {
+		if (!ligado) { // se nao estiver ligado
 			return 0;
-		} else {
+		} else { // se estiver ligado
 			return (int) Math.round(fatorInjecao * 3000);
 		}
 	}
