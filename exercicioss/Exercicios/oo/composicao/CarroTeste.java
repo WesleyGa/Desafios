ab82407a676b8ea7b52ecc04f91c7bacce24d00d
wesleygah <wes16062002@gmail.com>
2023-09-20 17:10:30 -0300

    testes

--- exercicioss/Exercicios/oo/composicao/CarroTeste.java
+++ exercicioss/Exercicios/oo/composicao/CarroTeste.java
@@ -4,20 +4,23 @@ public class CarroTeste {
 	public static void main(String[] args) {
 
 		Carro c = new Carro();
-
-		System.out.println(c.estaLigado());
+		
 		c.ligar();
-		System.out.println(c.estaLigado());
-
-		System.out.println(c.motor.gitros());
+	
 
 		c.acelerar();
 		c.acelerar();
-		c.acelerar();
-		c.acelerar();
+		System.out.println(c.motor.rpm());
+		c.frear();
+		c.frear();
+		c.frear();
+		c.frear();
+		c.frear();
+		c.frear();
+		System.out.println(c.motor.rpm());
 
-		System.out.println(c.motor.gitros());
-
+		System.out.println(c.estaLigadp());
+        System.out.println(c.motor.fatorInjecao);
 	}
 
 }
