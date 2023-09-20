5e2d6e7825eb1806dd4061374baac527199f64ca
wesleygah <wes16062002@gmail.com>
2023-09-20 17:09:08 -0300

    Revisão sobre composição.

--- exercicioss/Exercicios/oo/composicao/Carro.java
+++ exercicioss/Exercicios/oo/composicao/Carro.java
@@ -1,32 +1,44 @@
 package oo.composicao;
 
 public class Carro {
-	
-	 Motor motor;
 
-	Carro(){
+	Motor motor;
+
+	/*
+	 * Nesse caso, essa parte new Motor(this), literalmente está sendo passado como
+	 * parâmetro para motor this, que nada mais seria que a classe atual, ou seja,
+	 * um carro.
+	 */
+
+	Carro() {
 		this.motor = new Motor(this);
 	}
 
+	// metedo para acelerar o carro
 	void acelerar() {
 		motor.fatorInjecao += 0.4;
+
 	}
 
+	// frear
 	void frear() {
-		motor.fatorInjecao -= 0.4;
+		if (motor.fatorInjecao > 0.4) {
+			motor.fatorInjecao -= 0.4;
+		}
 	}
 
+	// ligar
 	void ligar() {
 		motor.ligado = true;
-
 	}
 
+	// desligar
 	void desligar() {
 		motor.ligado = false;
 	}
 
-	boolean estaLigado() {
+	// vereficar se esta ligado
+	boolean estaLigadp() {
 		return motor.ligado;
 	}
-
 }
