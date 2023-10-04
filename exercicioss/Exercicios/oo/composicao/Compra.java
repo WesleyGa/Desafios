5cca7e4fa3be8e50a60743c7feb2154f9517cc28
wesleygah <wes16062002@gmail.com>
2023-10-04 14:32:13 -0300

    Acréscimos de metedos e testes.

--- exercicioss/Exercicios/oo/composicao/Compra.java
+++ exercicioss/Exercicios/oo/composicao/Compra.java
@@ -3,18 +3,26 @@ package oo.composicao;
 import java.util.ArrayList;
 
 public class Compra {
-	
-	String cliente;
 
-	ArrayList<Item> itens = new ArrayList<Item>();
+	String cliente;
 	
-	double obterValorTotal() {
+	ArrayList<Item> itens = new ArrayList<Item>(); // lista de itens da compra
+
+	
+	void adicionarItem(Item item) { // metedo para adiconar os dados na lista.
+		this.itens.add(item);
+		item.compra = this;
+	}
+
+	
+	double valorTotal() { // para calcular o preço total de cada compra feita
 		double total = 0;
-		
-		for (Item item: itens) {
-			total += item.quantidade * item.preco;
+
+		for (Item item : itens) {
+
+			total += item.preco * item.quantidade;
 		}
-		
+
 		return total;
 	}
 
--- exercicioss/Exercicios/oo/composicao/CompraTeste.java
+++ exercicioss/Exercicios/oo/composicao/CompraTeste.java
@@ -2,14 +2,43 @@ package oo.composicao;
 
 public class CompraTeste {
 	public static void main(String[] args) {
-		Compra compra1 = new Compra();
-		compra1.cliente = "Joao Pedro";
-		compra1.itens.add(new Item("Caneta", 10, 1.50));
-		compra1.itens.add(new Item("Borracha", 3, 7.10));
-		compra1.itens.add(new Item("Caderno", 8, 3.55));
-		
-		System.out.println(compra1.itens.size());
-		System.out.println(compra1.obterValorTotal() );
+		Compra c1 = new Compra();
+		c1.cliente = "wesley";
+		// adicionando com o métedo de adicionar itens
+		c1.adicionarItem(new Item("caneta", 4, 7.45));
+		c1.adicionarItem(new Item("lapis", 9, 7));
+		c1.adicionarItem(new Item("jaqueta", 6, 9.4));
+
+		Compra c2 = new Compra();
+		c2.cliente = "gabriel";
+		// adicionando com o metedo do array
+		c2.itens.add(new Item("carrinho", 2, 9.81));
+		c2.itens.add(new Item("caderno", 10, 9));
+		c2.itens.add(new Item("corretivo", 19, 200));
+
+		System.out.println("Cliente : " + c1.cliente);
+		for (Item itens : c1.itens) {
+			System.out.println("Item " + c1.itens.indexOf(itens) + " : " + itens.nome);
+			System.out.println("Quantidade de " + itens.nome + " : " + itens.quantidade);
+			System.out.println("Preço da " + itens.nome + " : " + itens.preco);
+			System.out.println("-----------------------------------------------------------");
+		}
+		System.out.println("Valor total do carrinho de " + c1.cliente + " : " + c1.valorTotal());
+		System.out.println("***************************************************************");
+		System.out.println("***************************************************************");
+
+		System.out.println("Cliente : " + c2.cliente);
+		for (Item itens : c2.itens) {
+			System.out.println("Item " + c2.itens.indexOf(itens) + " : " + itens.nome);
+			System.out.println("Quantidade de " + itens.nome + " : " + itens.quantidade);
+			System.out.println("Preço da " + itens.nome + " : " + itens.preco);
+			System.out.println("-----------------------------------------------------------");
+		}
+
+		System.out.println("Valor total do carrinho de " + c2.cliente + " : " + c2.valorTotal());
+		System.out.println("***************************************************************");
+		System.out.println("***************************************************************");
+
 	}
 
 }
